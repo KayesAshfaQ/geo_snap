640b78cd58ba1d5cb235f7c6065d9570ef087701
KayesAshfaq <ashfaq7074@gmail.com>
2026-05-17 15:57:38 +0600

    Add StorageKeys class for attendance feature constants

---
+++ lib/src/core/constants/storage_keys.dart
@@ -0,0 +1,7 @@
+class StorageKeys {
+  StorageKeys._();
+
+  // Attendance Feature
+  static const String officeLocation = 'office_location';
+  static const String lastMarkedDate = 'last_marked_date';
+}
