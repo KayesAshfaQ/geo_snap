d64b9cd85119d33d3cac0194b47f28c96399dda7
KayesAshfaq <ashfaq7074@gmail.com>
2026-05-18 15:39:55 +0600

    Add pendingSnaps key for Snap feature in storage_keys

--- lib/src/core/constants/storage_keys.dart
+++ lib/src/core/constants/storage_keys.dart
@@ -3,5 +3,7 @@ class StorageKeys {
 
   // Attendance Feature
   static const String officeLocation = 'office_location';
-  static const String lastMarkedDate = 'last_marked_date';
+
+  // Snap Feature
+  static const String pendingSnaps = 'pending_snaps';
 }
