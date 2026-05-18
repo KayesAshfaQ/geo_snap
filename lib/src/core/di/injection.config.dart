7eb11fdd51b8450e549c5d7cc1c711442516b759
KayesAshfaq <ashfaq7074@gmail.com>
2026-05-18 15:43:17 +0600

    Update dependency injection and routing for Snap feature

--- lib/src/core/di/injection.config.dart
+++ lib/src/core/di/injection.config.dart
@@ -9,22 +9,36 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i3;
-import 'package:geo_snap/src/core/di/register_module.dart' as _i13;
+import 'package:geo_snap/src/core/di/register_module.dart' as _i20;
 import 'package:geo_snap/src/core/services/location_service.dart' as _i4;
 import 'package:geo_snap/src/features/attendance/data/datasources/attendance_local_data_source.dart'
-    as _i6;
-import 'package:geo_snap/src/features/attendance/data/repositories/attendance_repository_impl.dart'
-    as _i8;
-import 'package:geo_snap/src/features/attendance/domain/repositories/attendance_repository.dart'
     as _i7;
-import 'package:geo_snap/src/features/attendance/domain/usecases/get_office_location.dart'
+import 'package:geo_snap/src/features/attendance/data/repositories/attendance_repository_impl.dart'
     as _i9;
-import 'package:geo_snap/src/features/attendance/domain/usecases/mark_attendance.dart'
+import 'package:geo_snap/src/features/attendance/domain/repositories/attendance_repository.dart'
+    as _i8;
+import 'package:geo_snap/src/features/attendance/domain/usecases/get_office_location.dart'
     as _i10;
-import 'package:geo_snap/src/features/attendance/domain/usecases/save_office_location.dart'
+import 'package:geo_snap/src/features/attendance/domain/usecases/mark_attendance.dart'
     as _i11;
+import 'package:geo_snap/src/features/attendance/domain/usecases/save_office_location.dart'
+    as _i15;
 import 'package:geo_snap/src/features/attendance/presentation/bloc/attendance_bloc.dart'
+    as _i16;
+import 'package:geo_snap/src/features/snap/data/datasources/snap_local_data_source.dart'
+    as _i6;
+import 'package:geo_snap/src/features/snap/data/repositories/snap_repository_impl.dart'
+    as _i13;
+import 'package:geo_snap/src/features/snap/domain/repositories/media_repository.dart'
     as _i12;
+import 'package:geo_snap/src/features/snap/domain/usecases/get_pending_uploads.dart'
+    as _i17;
+import 'package:geo_snap/src/features/snap/domain/usecases/save_captured_image.dart'
+    as _i14;
+import 'package:geo_snap/src/features/snap/presentation/bloc/snap/snap_bloc.dart'
+    as _i18;
+import 'package:geo_snap/src/features/snap/presentation/bloc/sync/sync_bloc.dart'
+    as _i19;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:shared_preferences/shared_preferences.dart' as _i5;
@@ -47,24 +61,38 @@ extension GetItInjectableX on _i1.GetIt {
       () => registerModule.sharedPreferences,
       preResolve: true,
     );
-    gh.lazySingleton<_i6.AttendanceLocalDataSource>(
-        () => _i6.AttendanceLocalDataSourceImpl(gh<_i5.SharedPreferences>()));
-    gh.lazySingleton<_i7.AttendanceRepository>(() =>
-        _i8.AttendanceRepositoryImpl(gh<_i6.AttendanceLocalDataSource>()));
-    gh.factory<_i9.GetOfficeLocation>(
-        () => _i9.GetOfficeLocation(gh<_i7.AttendanceRepository>()));
-    gh.factory<_i10.MarkAttendance>(
-        () => _i10.MarkAttendance(gh<_i7.AttendanceRepository>()));
-    gh.factory<_i11.SaveOfficeLocation>(
-        () => _i11.SaveOfficeLocation(gh<_i7.AttendanceRepository>()));
-    gh.factory<_i12.AttendanceBloc>(() => _i12.AttendanceBloc(
-          getOfficeLocation: gh<_i9.GetOfficeLocation>(),
-          saveOfficeLocation: gh<_i11.SaveOfficeLocation>(),
-          markAttendance: gh<_i10.MarkAttendance>(),
+    gh.lazySingleton<_i6.SnapLocalDataSource>(
+        () => _i6.SnapLocalDataSourceImpl(gh<_i5.SharedPreferences>()));
+    gh.lazySingleton<_i7.AttendanceLocalDataSource>(
+        () => _i7.AttendanceLocalDataSourceImpl(gh<_i5.SharedPreferences>()));
+    gh.lazySingleton<_i8.AttendanceRepository>(() =>
+        _i9.AttendanceRepositoryImpl(gh<_i7.AttendanceLocalDataSource>()));
+    gh.factory<_i10.GetOfficeLocation>(
+        () => _i10.GetOfficeLocation(gh<_i8.AttendanceRepository>()));
+    gh.factory<_i11.MarkAttendance>(
+        () => _i11.MarkAttendance(gh<_i8.AttendanceRepository>()));
+    gh.lazySingleton<_i12.MediaRepository>(
+        () => _i13.SnapRepositoryImpl(gh<_i6.SnapLocalDataSource>()));
+    gh.factory<_i14.SaveCapturedImage>(
+        () => _i14.SaveCapturedImage(gh<_i12.MediaRepository>()));
+    gh.factory<_i15.SaveOfficeLocation>(
+        () => _i15.SaveOfficeLocation(gh<_i8.AttendanceRepository>()));
+    gh.factory<_i16.AttendanceBloc>(() => _i16.AttendanceBloc(
+          getOfficeLocation: gh<_i10.GetOfficeLocation>(),
+          saveOfficeLocation: gh<_i15.SaveOfficeLocation>(),
+          markAttendance: gh<_i11.MarkAttendance>(),
           locationService: gh<_i4.LocationService>(),
         ));
+    gh.factory<_i17.GetPendingUploads>(
+        () => _i17.GetPendingUploads(gh<_i12.MediaRepository>()));
+    gh.factory<_i18.SnapBloc>(() => _i18.SnapBloc(
+          gh<_i14.SaveCapturedImage>(),
+          gh<_i17.GetPendingUploads>(),
+        ));
+    gh.factory<_i19.SyncBloc>(
+        () => _i19.SyncBloc(getPendingUploads: gh<_i17.GetPendingUploads>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i13.RegisterModule {}
+class _$RegisterModule extends _i20.RegisterModule {}
--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/attendance/presentation/pages/attendance_page.dart';
+import '../../features/snap/presentation/pages/snap_preview_screen.dart';
+import '../../features/snap/presentation/pages/upload_manager_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/attendance',
@@ -16,6 +18,14 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/attendance',
       builder: (context, state) => const AttendancePage(),
+    ),
+    GoRoute(
+      path: '/snap',
+      builder: (context, state) => const SnapPreviewScreen(),
+    ),
+    GoRoute(
+      path: '/upload_manager',
+      builder: (context, state) => const UploadManagerScreen(),
     ),
   ],
 );
