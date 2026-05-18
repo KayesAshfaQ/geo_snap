a1dce041dd5c35e57464ea587cb6515b31ebd12f
KayesAshfaq <ashfaq7074@gmail.com>
2026-05-18 15:41:36 +0600

    Initialize Workmanager and update main app theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:workmanager/workmanager.dart';
 import 'src/core/di/injection.dart';
 import 'src/core/router/app_router.dart';
 import 'src/core/theme/app_theme.dart';
+import 'src/features/snap/data/datasources/snap_sync_worker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Dependency Injection
   await configureDependencies();
-  
+
+  // Initialize Workmanager
+  await Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+
   runApp(const MainApp());
 }
 
@@ -21,6 +26,8 @@ class MainApp extends StatelessWidget {
       title: 'GeoSnap',
       routerConfig: appRouter,
       theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
     );
   }
